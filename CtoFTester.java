de9a47ddfbd85dc28612bfb74af96f7f77c7dff0
Sadi Nirloy <snirloy30@stuy.edu>
2021-09-23 20:06:06 -0400

    Added the comment and function shells that produced no errors.

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,4 +1,15 @@
+/* These two functions should accept a double and return a double. This allows for 
+more accurate conversions. Furthermore, there is a fraction involved : 9/5 and 5/9, but if 
+they are written that way, as integers, the output will be 1 and 0 respectively. */
 public class CtoFTester{
+	public static double celsiusToFahrenheit(double celsius){
+		return 1.0;
+	}
+	
+	public static double fahrenheitToCelsius(double fahrenheit){
+		return 1.0;
+	}
+	
 	public static void main(String[]args){
 		System.out.println("Hello, world!");
 	}
